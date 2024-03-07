8507920c7642eff2cbd16e8f0d1e9f110ec711c4
A S Harish Kumar <107850160+ASHarish001@users.noreply.github.com>
2024-03-07 13:07:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function calculate(){
+    var principalElement = document.getElementById("principal");
+    var interestElement = document.getElementById("interest");
+    var yearsElement = document.getElementById("years");
+    var totalElement = document.getElementById("total");
+    var interestEarned = document.getElementById("interest-earned");
+
+    var p = Number(principalElement.value);
+    var r = Number(interestElement.value);
+    var t = Number(yearsElement.value);
+
+    if(p < 0 || isNaN(p)){
+        // alert("Please enter valid amount !");
+        p = 0;
+        principalElement.value = 0;
+    }
+
+    if(t < 0 || isNaN(t)){
+        t = 0;
+        yearsElement.value = 0;
+        // alert("Please enter valid value for years !");
+    }
+
+    if(r < 0 || isNaN(r)){
+        r = 0;
+        interestElement.value = 0;
+        // alert("Please enter valid interest rate !");
+    }
+
+    var interestAmt = Number((p * t * r) / 100);
+    var totalAmount = Number(interestAmt + p);
+
+    totalElement.textContent = totalAmount.toLocaleString(undefined, {style: "currency", currency: "INR"});
+    interestEarned.textContent = interestAmt.toLocaleString(undefined, {style: "currency" , currency: "INR"});
+}
